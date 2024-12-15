e3d1ece0ea2b0c77401c64896ed2de91b1b9b189
Neto test <balaz0@yandex.ru>
2024-12-15 12:04:55 +0300

    BonusMilesService

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,8 +1,8 @@
 public class BonusMilesService {
 
-    public int calculate (int a) {
+    public int calculate(int price) {
 
-        int result = a / 20;
+        int result = price / 20;
 
         return result;
     }
